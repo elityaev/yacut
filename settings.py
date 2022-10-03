3944b037999ee8a1acb1ae5fe45c6eadb25d3111
Евгений Литяев <elityaev@yandex.ru>
2022-10-03 10:25:33 +0300

    Отправка на ревью 2

--- settings.py
+++ settings.py
@@ -5,6 +5,7 @@ LEN_SHORT_ID = 6
 MIN_LEN_CUSTOM_ID = 1
 MAX_LEN_CUSTOM_ID = 16
 
+
 class Config(object):
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
